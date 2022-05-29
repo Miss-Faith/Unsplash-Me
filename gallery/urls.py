c0a343fa39062ace1724779c04f69d61c442b158
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-05-30 00:17:26 +0300

    Update gallery urls

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
-  
+    path('',views.home,name = 'home'),
+    path('search/', views.search_image, name='search_image'),
+    path('location/(?P<image_location>\d+)', views.location_filter, name='location_filter'),
+    path('image/(?P<category_name>\w+)/(?P<image_id>\d+)',views.single_category,name = 'single')
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
