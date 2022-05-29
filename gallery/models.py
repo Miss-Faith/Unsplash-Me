b89cb41c7d7201d2eef8b33b55648a1b76743afd
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-05-29 19:27:42 +0300

    Add class location

--- gallery/models.py
+++ gallery/models.py
@@ -40,3 +40,24 @@ class Image(models.Model):
 
     class Meta:
         ordering = ['date']
+
+class Location(models.Model):
+    name = models.CharField(max_length=60)
+
+    @classmethod
+    def get_locations(cls):
+        locations = Location.objects.all()
+        return locations
+
+    def __str__(self):
+        return self.name
+
+    @classmethod
+    def update_location(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
